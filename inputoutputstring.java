547090343ec3beae684193188069d58f683fa269
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-03-24 22:45:02 +0530

    Add files via upload

---
+++ inputoutputstring.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+public class inputoutputstring {
+public static void main(String args[])
+{
+    String str="abcd";
+    String str2=new String("xyz");
+    // string is immutable
+    //user input String
+    Scanner sc=new Scanner(System.in);
+    String name=sc.nextLine();
+    System.out.println(name);
+
+}
+}
