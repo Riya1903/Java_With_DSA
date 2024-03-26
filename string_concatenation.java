aac46b9d3a1c7a2875a99c612d3ef8988fcddf47
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-03-26 23:20:46 +0530

    Add files via upload

---
+++ string_concatenation.java
@@ -0,0 +1,9 @@
+public class string_concatenation {
+public static void main(String args[])
+{///string concatenation
+    String firstname="Riya";
+    String lastname="Pawar";
+    String fullname=firstname+" "+lastname;
+    System.out.println(fullname);
+}
+}
