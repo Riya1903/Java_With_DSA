2ac6f49132c15ce754284185d6063649948a3008
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-03-27 23:02:54 +0530

    Add files via upload

---
+++ string_method.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class string_method {
+public static void main(String args[])
+{
+    String firstname="Riya";
+    String lastname="Pawar";
+    String fullname=firstname+" "+lastname;
+    System.out.println(fullname.charAt(3));
+}
+}
