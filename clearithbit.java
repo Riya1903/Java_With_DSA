fa39f8ee62daff2608895bbeae556638f24ab9fd
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-05-20 17:03:14 +0530

    Add files via upload

---
+++ clearithbit.java
@@ -0,0 +1,12 @@
+public class clearithbit {
+        public static int clearithbits(int n,int i)
+        {
+            int bitMask=~(1<<i);
+            return n&bitMask;
+        }
+        public static void main(String[] args) {
+            System.out.println(clearithbits(10,1));
+        }
+        
+        
+}
